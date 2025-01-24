2943147c0b1d3a992a338eba240b6b22a5104c66
Ritu Raj Pratap Singh <iamriturajps@gmail.com>
2025-01-24 15:49:57 +0530

    fix: update static file serving path in server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
 	allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
